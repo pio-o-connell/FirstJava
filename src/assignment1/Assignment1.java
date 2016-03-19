88737e4c94d381a627a1698bc830760bb20b0576
pio-o-connell <pio_o_con@hotmail.com>
2016-03-19 07:53:18 +0000

    Fourth Java app created - back in October. 2013 Now Beautified.

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -15,12 +15,13 @@ public class Assignment1 {
      * @param args no command line arguments
      */
     public static void main(String[] args) {
-        int i,nmrBottles=99;
-        
-	for ( i=nmrBottles;i > 1; i--)
-            System.out.println( i + "  Bottles on the wall \n"  + i + "  Bottles of beer \n   Take one down \n   Pass it around \n\n");
-	System.out.println( i + "   Bottle on the wall \n" + i + "   Bottle of beer \n    Take one down \n    Pass it around \n\n");
-	System.out.println( "No more bottles of beer on the wall");
+        int i, nmrBottles = 99;
+
+        for (i = nmrBottles; i > 1; i--) {
+            System.out.println(i + "  Bottles on the wall \n" + i + "  Bottles of beer \n   Take one down \n   Pass it around \n\n");
+        }
+        System.out.println(i + "   Bottle on the wall \n" + i + "   Bottle of beer \n    Take one down \n    Pass it around \n\n");
+        System.out.println("No more bottles of beer on the wall");
     }
-    
+
 }
